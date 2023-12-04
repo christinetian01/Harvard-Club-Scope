0469b49c067a0ae7f36766bf51fc5a328120c862
Christine_Tian <christinetian01@gmail.com>
2023-12-03 20:37:58 -0500

    checks for passed dates, added titles

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+# connect to clubs database on PostgreSQL
 conn = psycopg2.connect(database = "clubs", user = "postgres", password = "cs50harvardsc0pE2023", host = "localhost", port = "5432")
 db = conn.cursor()
 
@@ -34,39 +35,54 @@ def login_required(f):
 
 def club_scrape():
 
+    # scrape the CS website
     URL = "https://csadvising.seas.harvard.edu/opportunities/clubs/"
+    # 1. HTTP request that returns the HTML of the page
     page = requests.get(URL)
 
+    # 2. parses the HTML, creating a tree structure of the HTML data
     soup = BeautifulSoup(page.text)
 
+    # 3. can't access the club divs directly, so need to access the parent div first
     wrap_div = soup.find("div", {"class":"padding highlightable"})
+    # access the divs that contain the club information
     clubs = wrap_div.find("div", {"id":"body-inner"}).find_all('li')
 
+    # 4. for each of the different divs, extract the club name stored in an <a> element
     for club in clubs:
+        # 5. insert the extracted name into the club_names table if it doesn't already exist
         db.execute("INSERT INTO club_names SELECT (%s) WHERE NOT EXISTS (SELECT * FROM club_names WHERE clubname = (%s))", (club.find('a').text, club.find('a').text))
+        # commit the insertion
         conn.commit()
 
+# running everytime the page is opened in case the website is updated with new clubs
 club_scrape()
+
+# getting all of the club names
 db.execute("SELECT * FROM club_names")
 clubs = db.fetchall()
 
 
-
 @app.route("/", methods=["GET", "POST"])
-@app.route('/')
 def index():
+    
+    # 1. should generate years starting from current until current + 10years
     years = []
-    year = datetime.date.today().year
+    year = datetime.datetime.today().year
     for i in range(10):
         years.append(year+i)
 
+    # 2. render homepage with the default values
     if request.method == "GET":
-        return render_template("homepage.html", date = datetime.date.today(), years = years, message = "Click to see events!")
+        return render_template("homepage.html", date = '', years = years, message = "Click to see events!")
     
+    # 3. render homepage for the selected date
     date = request.form.get('cell')
+    # 4. get all the events that are on the selected date
     db.execute("SELECT * FROM events WHERE date = %s ORDER BY time", (date, ))
     events = db.fetchall()
 
+    # 5. render homepage with the relevant information
     return render_template("homepage.html", date = date, years = years, events = events, message = "Events for " + date)
     
 
@@ -77,37 +93,36 @@ def login():
     # Forget any user_id
     session.clear()
 
-    # User reached route via POST (as by submitting a form via POST)
-    if request.method == "POST":
-        # Ensure username was submitted
-        if not request.form.get("clubname"):
-            return render_template("error.html", message = "must provide club name")
-
-        # Ensure password was submitted
-        elif not request.form.get("password"):
-            return render_template("error.html", message = "must provide password")
-
-        # Query database for username
-        db.execute(
-            "SELECT * FROM registered_clubs WHERE clubname = %s", (request.form.get("clubname"),)
-        )
-        rows = db.fetchall()
-
-        # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(
-            rows[0][3], request.form.get("password")
-        ):
-            return render_template("error.html", message = "invalid username and/or password")
-
-        # Remember which user has logged in
-        session["user_id"] = rows[0][0]
-
-        # Redirect user to upcoming events
-        return redirect("/upcoming_events")
-
     # User reached route via GET (as by clicking a link or via redirect)
-    else:
+    if request.method == "GET":
         return render_template("login.html", clubs = clubs)
+
+    # User reached route via POST (as by submitting a form via POST)
+    # Ensure username was submitted
+    if not request.form.get("clubname"):
+        return render_template("error.html", message = "must provide club name")
+
+    # Ensure password was submitted
+    elif not request.form.get("password"):
+        return render_template("error.html", message = "must provide password")
+
+    # Query database for username
+    db.execute(
+        "SELECT * FROM registered_clubs WHERE clubname = %s", (request.form.get("clubname"),)
+    )
+    rows = db.fetchall()
+
+    # Ensure username exists and password is correct
+    if len(rows) != 1 or not check_password_hash(
+        rows[0][3], request.form.get("password")
+    ):
+        return render_template("error.html", message = "invalid username and/or password")
+
+    # Remember which user has logged in
+    session["user_id"] = rows[0][0]
+
+    # Redirect user to upcoming events
+    return redirect("/upcoming_events")
     
 
 
@@ -115,8 +130,11 @@ def login():
 def register():
     """Register user"""
 
+    # 1. if the request method is GET, render register.html
     if request.method == "GET":
         return render_template("register.html", clubs = clubs)
+
+    # 2. request method is POST
 
     # getting the username and confirmation
     # not storing password for cybersecurity reasons
@@ -124,7 +142,8 @@ def register():
     confirm = request.form.get("confirmation")
     bio = request.form.get("bio")
 
-    # username wasn't entered
+    # 3. input validation
+    # clubname wasn't entered
     if not clubname:
         return render_template("error.html", message = "Please select a club!")
     # password wasn't entered
@@ -139,17 +158,19 @@ def register():
     if request.form.get("password") != confirm:
         return render_template("error.html", message = "Confirmation password didn't match! Please re-enter!")
 
-    # hash the password
+    # 4. hash the password
     hash_pass = generate_password_hash(request.form.get("password"))
 
-    # insert new username and password into users table
+    # 5. insert new username and password into users table
     try:
         db.execute("INSERT INTO registered_clubs (clubname, bio, password) VALUES (%s, %s, %s)", (clubname, bio, hash_pass))
         conn.commit()
+    # 6. error if the club has already registered
     except psycopg2.errors.UniqueViolation:
         conn.rollback()
         return render_template("error.html", message = "This club has already registered")
 
+    # 7. redirect to homepage
     return redirect('/')
 
 
@@ -157,9 +178,12 @@ def register():
 @app.route("/add_event", methods = ["GET", "POST"])
 @login_required
 def add_event():
+
+    # 1. if the user is routed to the page, then redner the add_event template
     if request.method == "GET":
         return render_template("add_event.html")
 
+    # 2. POST
     event_name = request.form.get("event_name")
     description = request.form.get("description")
     loc = request.form.get("loc")
@@ -167,6 +191,7 @@ def add_event():
     date = request.form.get("date")
     time = request.form.get("time")
 
+    # 3. input validation
     if not event_name:
         return render_template("error.html", message = "Please provide an event name")
     if not description:
@@ -180,9 +205,19 @@ def add_event():
     if not time:
         return render_template("error.html", message = "Please provide a time")
     
+    # check if the date has already passed
+    full_date_time = date +" "+time +":00"
+    date_time = datetime.datetime.strptime(full_date_time.replace('-', '/'), '%Y/%m/%d %H:%M:%S')
+
+    if date_time < datetime.datetime.today():
+        return render_template("error.html", message = "That date/time has already passed!")
+    
+    # 4. get the clubname of the one that submitted the form
     db.execute("SELECT clubname FROM registered_clubs WHERE id = %s", (session["user_id"], ))
     club_name = db.fetchall()[0]
 
+    # 5. insert the event information into the events table
+    # each event will have an event id
     db.execute("INSERT INTO events VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", (session["user_id"], club_name, event_name, description, loc, room_number, date, time))
     conn.commit()
 
@@ -197,63 +232,52 @@ def logout():
     # Forget any user_id
     session.clear()
 
-    # Redirect user to login form
+    # Redirect user to homepage
     return redirect("/")
-
-if __name__ == "__main__":
-    app.run(debug = True)
-
-
 
 
 @app.route("/upcoming_events", methods = ["GET"])
 @login_required
 def upcoming_events():
+    # selects events that are on the same day or on following days
+    # only shows the club events
     db.execute("SELECT * FROM events WHERE club_id = %s AND date >= (SELECT date_trunc('day', NOW())) ORDER BY date, time", (session["user_id"], ))
     events = db.fetchall()
 
     return render_template("upcoming_events.html", events = events)
 
 
-
-@app.route("/club_profile", methods = ["POST"])
-def club_profile():
-    club = request.form.get('club_button')
-
-    db.execute("SELECT * FROM registered_clubs WHERE clubname = %s", (club, ))
-    club_info = db.fetchall()[0]
-    print(club_info)
-
-    return render_template("club_profile.html", club_info = club_info)
-
-
-
 @app.route("/edit_bio", methods = ["GET", "POST"])
 @login_required
 def edit_bio():
         if request.method == "GET":
+            # retrieve the current bio
             db.execute("SELECT bio FROM registered_clubs WHERE id = %s", (session["user_id"], ))
             bio = db.fetchall()[0]
 
+            # displays the current bio
             return render_template("edit_bio.html", bio = bio)
         
+        # get new bio
         new_bio = request.form.get("new_bio")
+        # make sure new bio is not empty
         if not new_bio:
             return render_template("error.html", message = "Bio cannot be empty")
 
+        # update registered_clubs with the new bio
         db.execute("UPDATE registered_clubs SET bio = %s WHERE id = %s", (new_bio, session["user_id"]))
         conn.commit()
 
         return redirect("/")
 
 
-
-
 @app.route("/edit_events_direct", methods = ["POST"])
 @login_required
 def edit_events_direct():
+    # get the id of the event they want to change
     event_id = request.form.get("edit_button")
     
+    # retrieve the event with the event_id
     db.execute("SELECT * FROM events WHERE event_id = %s", (event_id,))
     event = db.fetchall()[0]
 
@@ -264,8 +288,10 @@ def edit_events_direct():
 @app.route("/edit_events", methods = ["POST"])
 @login_required
 def edit_events():
+    # get the event id
     event_id = request.form.get("event_id")
 
+    # get the form inputs
     event_name = request.form.get("event_name")
     description = request.form.get("description")
     loc = request.form.get("loc")
@@ -273,6 +299,7 @@ def edit_events():
     date = request.form.get("date")
     time = request.form.get("time")
 
+    # input validation
     if not event_name:
         return render_template("error.html", message = "Please provide an event name")
     if not description:
@@ -285,21 +312,36 @@ def edit_events():
         return render_template("error.html", message = "Please provide a date")
     if not time:
         return render_template("error.html", message = "Please provide a time")
+    
+    # check if the date has already passed
+    full_date_time = date +" "+time +":00"
+    date_time = datetime.datetime.strptime(full_date_time.replace('-', '/'), '%Y/%m/%d %H:%M:%S')
+    
+    if date_time < datetime.datetime.today():
+        return render_template("error.html", message = "That date/time has already passed!")
 
+    # update the event corresponding to the event id
     db.execute("UPDATE events SET name = %s, description = %s, locationname = %s, room_num = %s, date = %s, time = %s WHERE event_id = %s", (event_name, description, loc, room_number, date, time, event_id))
     conn.commit()
 
     return redirect("/upcoming_events")
 
+
+
 @app.route("/explore_clubs", methods = ["GET"])
 def explore_clubs():
-    db.execute("SELECT * FROM registered_clubs")
+    # selects all registered clubs
+    db.execute("SELECT clubname, bio FROM registered_clubs")
     clubs = db.fetchall()
 
+    # displays all the registered clubs
     return render_template("explore_clubs.html", clubs = clubs)
+
+
 
 @app.route("/all_upcoming_events", methods = ["GET"])
 def all_upcoming_events():
+
     db.execute("SELECT * FROM events WHERE date >= (SELECT date_trunc('day', NOW())) ORDER BY date, time")
     all_events = db.fetchall()
 
--- static/calendar.js
+++ static/calendar.js
@@ -1,85 +1,109 @@
+// 1. only run once the HTML is loaded
 document.addEventListener('DOMContentLoaded', function(){
 
     const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
+    // 2. the default date for the calendar is the current date
     const currentDate = new Date();
     let selectedYear = currentDate.getFullYear();
     let selectedMonth = currentDate.getMonth();
 
+    // 3. get the selected from the dropdown menus
+    // selected date should be empty when the page is first opened
     let selectedDate = document.querySelector("#selected_date").innerHTML;
-    console.log(selectedDate);
 
+    // if selectedDate is not empty, then set the selected year and month to the value from the #selected_date element
+    // defaults to current day if no day has been inputted yet
     if (selectedDate !=""){
         let newDate = new Date(selectedDate);
         selectedYear = newDate.getFullYear();
         selectedMonth = newDate.getMonth();
     }
-    console.log(selectedYear);
-    console.log(selectedMonth);
 
-
+    // 4. select element for the month
     const month_select = document.querySelector("#month")
+    // select element for the year
     const year_select = document.querySelector("#year")
     
     function calendarGrid(month, year){
+        // 5. change the headers to display the selected month and year
         document.getElementById("month_text").innerHTML = months[month];
         document.getElementById("year_text").innerHTML = year;
     
+        // 6. select the div that is supposed to contain the actual calendar
         let cal_days = document.getElementById("calendar_days");
 
+        // if the calendary body already has elements in it, remove them all to start fresh
         while (cal_days.hasChildNodes()){
             cal_days.removeChild(cal_days.firstChild);
         }
 
+        // 7. the index of the first day of the month
         let firstDay = new Date(year, month, 1).getDay();
+        // the number of days in a month
         let days = new Date(year, month+1, 0).getDate()
         
-        // making the filler cells
+        // 8. making the filler cells
         for (let i = 0; i<firstDay; i++){
             let fillerDiv = document.createElement("div");
             fillerDiv.className = "filler";
 
+            // append to the calendar body
             cal_days.appendChild(fillerDiv)
         }
 
-        // making the actual cells
+        // 9. making the actual cells
         for (let i = 0; i<days; i++){
+            // each cell is a form/button that should send a post request to the homepage when clicked
+            // creating the form
             let cellDivForm = document.createElement("form")
             cellDivForm.action = "/"
             cellDivForm.className = "cell"
             cellDivForm.method = "post"
 
+            // creating the button that will make up the cell
             let cellDiv = document.createElement("button");
             cellDiv.className = "cell";
             cellDiv.name = "cell";
             cellDiv.type = "submit";
+
+            // 10. the value of the cell (button) will be the date that it corresponds to in the datetime format
+            // datetime format: YYYY-MM-DD
             if ((""+(i+1)).length == 1){
+                // need to add a zero if the day is only one digit
                 cellDiv.value = "" + year + "-" + (month+1) + "-0" + (i+1);
             }
             else{
                 cellDiv.value = "" + year + "-" + (month+1) + "-" + (i+1);
-            }    
+            }
+            // display the day on the button    
             cellDiv.innerHTML = i+1;
+            // add the button to the form
             cellDivForm.appendChild(cellDiv)
 
+            // 11. if there are already 7 elements (including the filler divs) in one row, then add a break element to go to the next row
             if (i%7 == (7-firstDay)){
                 nl = document.createElement("br");
                 cal_days.appendChild(nl);
             }
 
+            // 12.append the form to the calendary body
             cal_days.appendChild(cellDivForm);
         }
         
     }
 
 
+    // 13. function that regenerates the calendar when the month is changed
     function changeMonth(event){
         if (event.target){
+            // the month needs to be translated into an index
             selectedMonth = months.indexOf(event.target.value);
         }
         calendarGrid(selectedMonth, selectedYear)
     }
 
+    // 14. function that regenerates the calendar when the year is changed
     function changeYear(event){
         if (event.target){
             selectedYear = event.target.value;
@@ -87,12 +111,9 @@ document.addEventListener('DOMContentLoaded', function(){
         calendarGrid(selectedMonth, selectedYear)
     }
 
+    // 15. adding event listeners to the dropdowns so that the calendar changes according to the month and year input
     month_select.addEventListener("change", changeMonth);
     year_select.addEventListener("change", changeYear);
-
-
-    sessionStorage.setItem(selectedYear, selectedYear);
-    sessionStorage.setItem(selectedMonth, selectedMonth);
 
     calendarGrid(selectedMonth, selectedYear);
 })
